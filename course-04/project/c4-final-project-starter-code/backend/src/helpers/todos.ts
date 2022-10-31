b84d3d34c5a9b4ac31d2f70d55fbae8b258dd4b9
evans-manyala <evans.manyala@gmail.com>
2022-11-01 00:49:06 +0300

    Adding new files

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
@@ -1,10 +1,26 @@
-import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
+// import { TodosAccess } from './todosAcess'
+// import { AttachmentUtils } from './attachmentUtils';
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
-import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
-import { createLogger } from '../utils/logger'
+// import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+// import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
-import * as createError from 'http-errors'
+import { APIGatewayProxyEvent } from 'aws-lambda'
+import { getUserId } from '../lambda/utils'
+// import * as createError from 'http-errors'
 
-// TODO: Implement businessLogic
+
+// // TODO: Implement businessLogic
+export function todoBuilder(todoRequest: CreateTodoRequest,event:  APIGatewayProxyEvent): TodoItem{
+
+    const todoId = uuid.v4()
+    const todo = {
+      todoId: todoId,
+      createdAt: new Date().toISOString(),
+      userId: getUserId(event),
+      done: false,
+      attachmentUrl: '',
+      ...todoRequest
+    }
+    return todo as TodoItem
+}
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -3,16 +3,27 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
+import { getTodoById, updateTodo } from '../../helpers/todosAcess'
+import { getUploadUrl } from '../../helpers/attachmentUtils'
 
-import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
-import { getUserId } from '../utils'
+const bucketname = process.env.ATTACHMENT_S3_BUCKET
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
-    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    
+    const todo = await getTodoById(todoId)
+    todo.attachmentUrl = `https://${bucketname}.s3.amazonaws.com/${todoId}`
 
+await updateTodo (todo);
+const url = await getUploadUrl(todoId)
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        "uploadUrl": url
+      })
+    }
+
+    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
     return undefined
   }
 )
